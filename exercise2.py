8d28bc6cf97a68dfb153ff9fb5d9aa28a96697ca
abdulaziz661 <ansariabdulaziz65@gmail.com>
2019-09-26 11:54:36 +0530

    abc

--- exercise2.py
+++ exercise2.py
@@ -1,3 +1,7 @@
 print ("hello, world!")
 print ("hello,\tworld")
 print ("hello,\nworld") 
+print ("hello")
+print ("world!")
+if y=10
+   z=5
---
+++ variables8.py
@@ -0,0 +1,32 @@
+a=1 
+print(a)
+b=2
+print(b)
+print(a)
+print(b)
+c='hello there'
+print(c)
+a=2
+b=1
+print(a,b)
+print(a)
+a=1
+b=a
+print(b)
+a=10
+b=5
+print(a)
+print(b)
+c="hello world"
+print(c)
+d="hey abdul!"
+print(d)
+e = a
+print(e)
+v1 = "first name"
+v2 = "second name"  
+print(v1)
+print(v2)
+temp = "v1"
+v2 = "v1"
+
