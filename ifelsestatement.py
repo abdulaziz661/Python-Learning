e905487c45896e3cdd3f2c5bc8a2ac2c03096ad6
abdulaziz661 <ansariabdulaziz65@gmail.com>
2019-09-27 14:16:33 +0530

    abd

---
+++ ifelsestatement.py
@@ -0,0 +1,18 @@
+print("Enter a numeric grade: ")
+grade = int(input ())
+if grade >=90: 
+    letterGrade = "A"
+elif grade  >= 80:
+    letterGrade = "B"
+elif grade >= 70:
+    letterGrade = "C"
+elif grade >= 60:
+    letterGrade = "D" 
+elif grade <=59:
+    letterGrade = 'F'    
+else:    
+    print ("Did not recognise input") 
+print ("Your letter grade is" + letterGrade) 
+
+            
+        
---
+++ ifstatement.py
@@ -0,0 +1,9 @@
+print("Enter hours worked:")
+hoursworked = int(input())
+rate = 25.00
+if hoursworked > 40: 
+    grosspay = hoursworked * rate
+print ("Gross pay:" + str(grosspay))    
+
+
+
