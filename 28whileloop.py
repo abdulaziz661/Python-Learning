bd2d8ef929751372160d5bb8fa56f796ac7d5ce2
abdulaziz661 <ansariabdulaziz65@gmail.com>
2019-09-30 14:09:23 +0530

    aaa

--- 28whileloop.py
+++ 28whileloop.py
@@ -2,3 +2,8 @@ number  = 1
 while number <= 10:
     print(number)
     number = number +1
+ a = ["banana", "apple", "microsoft" ] 
+ for element in a:
+     print(element)
+     print(element)
+     
---
+++ 30whileloops.py
@@ -0,0 +1,12 @@
+average = 0.0
+total = 0
+count = 0
+print("Enter a grade (-1 to quit): ")
+grade = int(input())
+while grade != -1:
+    total = total + grades
+    count = count + 1
+    print("Enter a grade (-1 to quit): ")
+    grade = int(input() )
+average = total / count
+print ("Average grade: "+ str(average))
