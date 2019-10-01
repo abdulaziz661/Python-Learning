5c9aa078e8df2271b9c2fbee0f47b171c5da9e9f
abdulaziz661 <ansariabdulaziz65@gmail.com>
2019-10-01 17:33:54 +0530

    uuu

---
+++ 37Forloops.py
@@ -0,0 +1,21 @@
+
+numbers = [1,2,3,4,5]
+total = 0
+for x in numbers:
+   total = total + x
+print(total) 
+
+
+for item2 in ['Mosh , John , Sara']:
+    print(item2)
+
+for item in range(5, 10, 2):
+    print(item)
+
+
+prices = [10, 20, 30]
+ 
+total = 0
+for price in prices:
+    total += price 
+print(total)
---
+++ 38forloops.py
@@ -0,0 +1,9 @@
+word = "hello"
+for letter in word:
+    print(letter)
+sentence = "now is the time for all good people to come to aid"   
+count = 0
+for letter in sentence:
+    if letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u':
+        count = count + 1
+print('The number of vowel is ' + str(count))        
